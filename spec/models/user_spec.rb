267568ac9eaa5f2ccc0fc5e6bf0a0cdc8d598c9a
unknown <sixu@CHN-SIXU.corp.microstrategy.com>
2012-07-11 15:19:29 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
@@ -99,4 +100,8 @@ describe User do
     end
   end
   
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
